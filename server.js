0d4b09fda621ae203c4176153d7789ce0e570e6f
fiyosa <fiyosa97@gmail.com>
2024-06-24 14:47:23 +0700

    fix: access db.json prod

--- server.js
+++ server.js
@@ -4,7 +4,10 @@ const generateImage = require('./generateImage')
 
 const port = process.env.PORT || 4000
 
-const router = jsonServer.router(path.join(__dirname, 'db.json')) // Path to db.json
+const router =
+  process.env.NODE_ENV === 'development'
+    ? jsonServer.router(path.join(__dirname, 'db.json')) // Access local db.json
+    : jsonServer.router(JSON.parse(fs.readFileSync(path.join(__dirname, 'db.json')))) // Read only db.json
 const server = jsonServer.create() // Express server
 const middlewares = jsonServer.defaults()
 
